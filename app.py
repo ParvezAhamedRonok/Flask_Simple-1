b298256f704344ec569121127a41f56ba6dcf15d
ParvezAhamedRonok <mdpervez946@gmail.com>
2025-06-24 17:36:45 +0600

    added Flask-web app

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'  # Replace with your own secret key
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if 'count' not in session:
+        session['count'] = 0
+
+    if request.method == 'POST':
+        action = request.form.get('action')
+        if action == 'increment':
+            session['count'] += 1
+        elif action == 'decrement':
+            session['count'] -= 1
+
+    return render_template('index.html', count=session['count'])
 
 if __name__ == '__main__':
     app.run(debug=True)
